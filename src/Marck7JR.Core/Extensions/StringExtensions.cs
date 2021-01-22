3e8686acea838758829335ebea6f98353519d654
Lucimarck J S Dias <lucimarck.dias@outlook.com>
2021-01-22 10:23:17 -0400

    Add StringExtensions

---
+++ src/Marck7JR.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Marck7JR.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string? @string) => string.IsNullOrEmpty(@string);
+        public static bool IsNullOrWhiteSpace(this string? @string) => string.IsNullOrWhiteSpace(@string);
+    }
+}
