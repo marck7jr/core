6b356237dfcc23b1697c19ef8b3bc33904e78fe0
Lucimarck J S Dias <lucimarck.dias@outlook.com>
2021-02-01 18:03:32 -0400

    Add ToObservableCollectionAsync extension method

--- src/Marck7JR.Core/Extensions/EnumerableExtensions.cs
+++ src/Marck7JR.Core/Extensions/EnumerableExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Marck7JR.Core.Extensions
 {
@@ -8,5 +9,6 @@ namespace Marck7JR.Core.Extensions
     {
         public static IEnumerable<T?> Reflect<T>(this IEnumerable<object> enumerable) => enumerable.Select(_ => _.Reflect<T>());
         public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> enumerable) => new(enumerable);
+        public static async ValueTask<ObservableCollection<T>> ToObservableCollectionAsync<T>(this IAsyncEnumerable<T> asyncEnumerable) => new(await asyncEnumerable.ToListAsync());
     }
 }
