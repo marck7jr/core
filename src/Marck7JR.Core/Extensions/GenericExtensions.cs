7ccfd66588740165022377cf49d910bd971cec58
Lucimarck J S Dias <lucimarck.dias@outlook.com>
2021-02-04 22:13:20 -0400

    Add GetCustomAttribute's generic extension methods

--- src/Marck7JR.Core/Extensions/GenericExtensions.cs
+++ src/Marck7JR.Core/Extensions/GenericExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
@@ -7,6 +8,7 @@ namespace Marck7JR.Core.Extensions
 {
     public static class GenericExtensions
     {
+        [Obsolete("Use 'GetCustomAttribute`2' instead.")]
         public static string GetDescription<T>(this T t)
         {
             DescriptionAttribute? attribute = null;
@@ -30,6 +32,32 @@ namespace Marck7JR.Core.Extensions
             }
 
             throw new NullReferenceException($"The field must have a {nameof(DescriptionAttribute)}.");
+        }
+
+        public static U GetCustomAttribute<T, U>(this T t, bool inherit = false) where U : Attribute => t.GetCustomAttributes<T, U>(inherit).FirstOrDefault();
+
+        public static IEnumerable<U> GetCustomAttributes<T, U>(this T? t, bool inherit = false)
+            where U : Attribute
+        {
+            if (t is null)
+            {
+                throw new ArgumentNullException(nameof(t));
+            }
+
+            MemberInfo memberInfo = t switch
+            {
+                Enum _ => t.GetType().GetField($"{t}"),
+                _ => t.GetType()
+            };
+
+            var attributes = memberInfo.GetCustomAttributes<U>(inherit);
+
+            if (attributes.Any())
+            {
+                return attributes;
+            }
+
+            throw new InvalidOperationException($"'{typeof(T).Name}' must have a {typeof(U).Name}.");
         }
 
         public static T? IfNotNull<T>(this T? value, Action<T>? action, Action? fallback = null) where T : class
